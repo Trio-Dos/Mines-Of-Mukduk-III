50bb2a8513947c64ae512d3e8377ea832c46dc19
Dustin Klein <dbklein4@gmail.com>
2022-08-15 21:01:11 -0500

    add Korge framework

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import com.soywiz.korge.gradle.*
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+	alias(libs.plugins.korge)
+}
+
+korge {
+	id = "io.triodos.mukduk"
+
+// To enable all targets at once
+	targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+	
+	targetJvm()
+//	targetJs()
+//	targetDesktop()
+//	targetIos()
+//	targetAndroidIndirect() // targetAndroidDirect()
+
+	serializationJson()
+	//targetAndroidDirect()
+}
