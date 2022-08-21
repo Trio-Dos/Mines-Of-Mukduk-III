0b05507e2dbb11a23a9f653abfb909fd31f67770
Dustin Klein <dbklein4@gmail.com>
2022-08-20 22:08:49 -0500

    change build target to only jvm and js

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,14 @@ korge {
 	id = "io.triodos.mukduk"
 
 // To enable all targets at once
-	targetAll()
+//	targetAll()
 
 // To enable targets based on properties/environment variables
 	//targetDefault()
 
 // To selectively enable targets
-	
 	targetJvm()
-//	targetJs()
+	targetJs()
 //	targetDesktop()
 //	targetIos()
 //	targetAndroidIndirect() // targetAndroidDirect()
